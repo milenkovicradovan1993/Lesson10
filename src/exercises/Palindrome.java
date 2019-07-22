602f50194896ba0f6d457e4d4a8d52548a86b49e
Radovan <Radovan1993@live.com>
2019-07-22 11:00:28 +0200

    Palindrome change

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -1,12 +1,30 @@
 package exercises;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BooleanSupplier;
 
 import examples.FileHelper;
 
 public class Palindrome {
+	List<String> words = new ArrayList<String>();
+
+	Palindrome() {
+
+		words = loadWords();
+	}
 
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
 	}
+
+	public boolean wordExist(String string) {
+
+		return words.contains(string);
+	}
+
+	public boolean wordIsAPalindrome(String string) {
+		return wordExist(string) && wordExist(new StringBuilder(string).reverse().toString());
+	}
+	
 }
